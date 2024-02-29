cc9b928dbf2d6abcb228e890c0ddf40051ef0296
KEL2649 <stacykelnhofer@northwesternmutual.com>
2024-02-29 07:57:30 -0600

    removes update from grafana

--- install.sh
+++ install.sh
@@ -82,7 +82,7 @@ check_and_install "grafana" "
   sudo mkdir -p /etc/apt/keyrings/ &&
   wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null &&
   echo 'deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main' | sudo tee -a /etc/apt/sources.list.d/grafana.list &&
-  sudo apt update && sudo apt install grafana 
+  sudo apt install grafana 
 "
 alert info "opening up grafana port 3000"
 sudo ufw allow 3000
