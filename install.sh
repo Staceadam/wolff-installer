f728a396e89e8ab1f493428e93c7da1e8e2d977c
KEL2649 <stacykelnhofer@northwesternmutual.com>
2024-02-29 07:58:46 -0600

    fixes bad echo

--- install.sh
+++ install.sh
@@ -81,7 +81,7 @@ sudo ufw allow 9100
 check_and_install "grafana" "
   sudo mkdir -p /etc/apt/keyrings/ &&
   wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null &&
-  echo 'deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main' | sudo tee -a /etc/apt/sources.list.d/grafana.list &&
+  echo deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main | sudo tee -a /etc/apt/sources.list.d/grafana.list &&
   sudo apt install grafana 
 "
 alert info "opening up grafana port 3000"
